339a6f71a3b3d8fad76e6976022b5613e4267c04
Claudia <clperez@scl.edu.gt>
2023-07-13 12:48:15 -0600

    Terminado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//Función asincrónica para recuperar los valores utilizando fetch
 async function getUsuarios() {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -9,19 +8,16 @@ async function getUsuarios() {
     }
   }
   
-  // Función para crear un componente individual por cada ítem de los datos
   function crearComponentes(data) {
     const container = document.getElementById('container');
   
-    // Utilizando un bucle for para iterar sobre los datos
+    //  un bucle for para iterar sobre los datos
     data.forEach(item => {
-      // Crear el componente individual
       const componente = document.createElement('div');
       componente.classList.add('caja');
 
      
   
-      // Agregar contenido al componente
 
 
       const profilePicture = document.createElement('div');
@@ -45,7 +41,6 @@ async function getUsuarios() {
       compañia.textContent = item.company.name;
       
   
-      // Agregar elementos al componente
       componente.appendChild(profilePicture);
       componente.appendChild(username);
       componente.appendChild(nombre);
@@ -53,12 +48,10 @@ async function getUsuarios() {
       componente.appendChild(telefono);
       componente.appendChild(compañia);
   
-      // Agregar componente al contenedor principal
       container.appendChild(componente);
     });
   }
   
-  // Llamar a la función asincrónica y crear los componentes cuando se obtengan los datos
   getUsuarios()
     .then(data => crearComponentes(data))
     .catch(error => console.log(error));
